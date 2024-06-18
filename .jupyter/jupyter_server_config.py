e76e7c11b89bd3c8b0c23ddb13dd302ad5d4a226
Elearning Polytechnique <eric.vantroeyen@polytechnique.edu>
2024-06-18 13:28:49 +0200

    Create jupyter_server_config.py

---
+++ .jupyter/jupyter_server_config.py
@@ -0,0 +1,19 @@
+import os
+from jupyter_server.services.config import ConfigManager
+
+def get_config():
+    cm = ConfigManager()
+    cm.update('notebook', {
+        "ContentsManager": {
+            "extra_static_paths": [os.path.abspath("files")]
+        }
+    })
+
+get_config()
+
+"""
+# Configuration file for notebook.
+
+c = get_config()  #noqa
+c.ExtensionApp.static_paths = ['/home/jovyan/files']
+"""
